1af0c28c61d79dd541ce4c44f2f2540bb9f1ec31
James Rose <jamesbrose123@gmail.com>
2013-07-28 22:08:19 +0100

    Update example with more explaination

--- examples/simple_scanner.rb
+++ examples/simple_scanner.rb
@@ -2,25 +2,33 @@ require 'ham'
 
 class SimpleScanner < Ham::Base
 
+  # Set your target hosts here. It will accept a network address with CIDR
+  # notation, a single address or an array of addresses.
+  #
+  # Example:
+  #   '10.10.10.1/28'
+  #   '192.158.5.2'
+  #   ['174.44.12.1', '12.4.4.5']
+  #
   def hosts
-    '10.10.10.1/28' # Accepts a network w/ CIDR notation
-    # Or a single address: 
-    # '192.158.5.2'
-    # Or an array of addresses:
-    # ['174.44.12.1', '12.4.4.5']
+    '10.10.10.1/28'
   end
 
+  # Set your HTTP headers here.
   def headers
-    {
-      'Referer' => 'http://www.google.com'
-    }
+    { 'Referer' => 'http://www.google.com' }
   end
 
+  # Set your request parameters here.
   def params
     { username: 'admin', password: 'admin' }
   end
 
-  # Each host is passed into a +scan+ method.
+  # Each host is passed into a +scan+ method in your scanner. Here you make the
+  # actual HTTP request. Your headers and parameters are injected automatically.
+  #
+  # Ham doesn't care what you do with the results of the request, but in this
+  # instance we check to see if the username is included in the response body.
   def scan(host)
     response = post("http://#{host}/path/to/app")
     if response
